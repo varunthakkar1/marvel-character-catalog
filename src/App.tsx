6bdc940a96df6f580a7b4f0b578c24a002e6b730
Varun Thakkar <varunthakkar2013@gmail.com>
2022-01-06 01:31:05 -0500

    added global styles and new font

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import HomePage from "./pages/home/HomePage";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import CharacterPage from "./pages/character/CharacterPage";
+import GlobalStyle from "./globalStyles";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <GlobalStyle/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
---
+++ src/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 20px;
+    font-family: Montserrat;
+    font-weight: bold;
+  }
+`;
+ 
+export default GlobalStyle;
