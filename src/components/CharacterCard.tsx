438520922a77cbbedd0f8aa1f8d8bc00c398bd6d
Varun Thakkar <varunthakkar2013@gmail.com>
2022-01-08 22:46:08 -0500

    character card updates

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -9,10 +9,16 @@ interface CharacterCardProps {
 const Container = styled.div`
   display: flex;
   width: 350px;
-  margin-bottom: 10px;
+  margin: 15px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
 `;
 
 const Thumbnail = styled.img`
@@ -21,15 +27,64 @@ const Thumbnail = styled.img`
   width: 200px;
   border-radius: 50%;
   object-fit: cover;
+  margin: 24px 0px;
 `;
+
+const CharacterName = styled.div`
+    display: flex;
+    font-weight: bolder;
+    font-weight: 20px;
+    margin-bottom: 16px;
+`;
+
+const CharacterDescription = styled.div`
+    display: flex;
+    width: 240px;
+    margin-bottom: 16px;
+    font-size: 13px;
+    text-align: center;
+    font-weight: light;
+`;
+
+const StatsContainer = styled.div`
+    display: flex;
+    width: 80%;
+    justify-content: space-evenly;
+    margin-bottom: 30px;
+`;
+
+const Stat = styled.div`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    width: 33%;
+    margin: 0px 1px;
+    font-size: 13px;
+`;
+
+const StatDivider = styled.div`
+    width: 1px;
+    height: 100%;
+    background-color: black;
+`; 
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
   return (
     <Container>
-      {character.name}
       <Thumbnail
         src={character.thumbnail.path + "." + character.thumbnail.extension}
       />
+      <CharacterName>{character.name}</CharacterName>
+      <CharacterDescription>
+          {character.description}
+      </CharacterDescription>
+      <StatsContainer>
+        <Stat>{character.events.available} event{character.events.available !== 1 && "s"}</Stat>
+        <StatDivider/>
+        <Stat>{character.comics.available} comic{character.comics.available !== 1 && "s"}</Stat>
+        <StatDivider/>
+        <Stat>{character.series.available} series</Stat>
+      </StatsContainer>
     </Container>
   );
 };
